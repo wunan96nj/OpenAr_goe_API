e051dea0287a288ed6fbd38c5d25828c4c1af6f5
wunan96nj <47093622+wunan96nj@users.noreply.github.com>
2022-03-31 11:59:54 -0400

    Update ClientTest.py

--- ClientTest.py
+++ ClientTest.py
@@ -1,28 +1,3 @@
-from collections import namedtuple
-import uuid
-from datetime import datetime
-import base64
-import requests
-import json
-
-
-def ConvertToBase64(src_filepath):
-    with open(src_filepath, 'rb') as imageFileAsBinary:
-        fileContent = imageFileAsBinary.read()
-        b64_encoded_img = base64.b64encode(fileContent)
-        return b64_encoded_img
-
-def create_geopose_request(img_file_base64_string, lat, lon):
-
-
-    """
-    Create request body for geopose
-    :param img_file_base64_string:
-    :param lat:
-    :param lon:
-    :return:
-    """
-    
     data = {
         "id": str(uuid.uuid4()),
         "timestamp": str(datetime.now()),
@@ -73,12 +48,18 @@ def create_geopose_request(img_file_base64_string, lat, lon):
 
 def main():
     api_url = "http://localhost:5666/capture-photo/getgeoposreq"
-    uploadImagePath = "images/P1180141.jpg"
+    image_name = "P1180141.jpg"
+    uploadImagePath = "../client/images/" + image_name
     img_file_base64_string = str(ConvertToBase64(uploadImagePath), 'utf-8')
     json_data = create_geopose_request(img_file_base64_string, 1, 1)
     ret = requests.post(api_url, data=json_data)
     ret = ret.json()
     print(ret)
+
+    json_data = json_data = json.dumps({"image_name": image_name})
+    requests.post("http://localhost:5666/capture-photo/goepose_gt", data=json_data)
+
+
     return
 
 if __name__ == '__main__':
